83b2b9780052b8ab66e5c4929864b7f552ebd228
korathbasil <korathbasil@live.com>
2020-07-08 00:24:30 +0530

    added memory support for standard calculator

--- Calculator.py
+++ Calculator.py
@@ -12,6 +12,24 @@ f2 = tkfont.Font(size = 20, weight = 'bold')
 f3 = tkfont.Font(size = 12, weight = 'bold')
 f4 = tkfont.Font(size = 24, weight = 'bold')
 
+def option_changed(*args):
+    x = var.get()
+    if x == "Standard":
+        scffrm.forget()
+        covfrm.forget()
+        stdfrm.pack()
+    elif x == "Scientific":
+        stdfrm.forget()
+        covfrm.forget()
+        scffrm.pack()
+    elif x == "Converter":
+        stdfrm.forget()
+        scffrm.forget()
+        covfrm.pack()
+
+## vriables for standard calculator ##
+##''''''''''''''''''''''''''''''''''##
+
 dispval = StringVar()
 dispoper = StringVar()
 value = ""
@@ -20,6 +38,17 @@ value1 = 0
 value2 = 0
 ans = 0
 anscheck = 0
+stdm = 0
+stdmemvar = StringVar()
+
+## vriables for scientific calculator ##
+##''''''''''''''''''''''''''''''''''''##
+
+scfdispval = StringVar()
+scfvalue = ""
+
+## functions for standard calculator ##
+##'''''''''''''''''''''''''''''''''''##
 
 def clrscr():
     global check, value1, value2, ans, anscheck
@@ -163,21 +192,36 @@ def result(y=0):
             dispoper.set("=")
         anscheck = 1
 
+def stdcheckmem(x):
+    global stdm, value, stdmemvar, dispval
+    if x == 3:
+        stdm = stdm + float(value)
+        stdmemvar.set("M")
+    elif x ==4:
+        stdm = stdm - float(value)
+        stdmemvar.set("M")
+    elif x == 2:
+        if stdm != 0:
+            value = str(stdm)
+            if value[len(value)-1] == '0':
+                x = len(value)
+                value = value[:x - 2]
+            dispval.set(value)
+    elif x ==1:
+        stdm = 0
+        stdmemvar.set("")
 
-def option_changed(*args):
-    x = var.get()
-    if x == "Standard":
-        scffrm.forget()
-        covfrm.forget()
-        stdfrm.pack()
-    elif x == "Scientific":
-        stdfrm.forget()
-        covfrm.forget()
-        scffrm.pack()
-    elif x == "Converter":
-        stdfrm.forget()
-        scffrm.forget()
-        covfrm.pack()
+## functions for scientific calculator ##
+##'''''''''''''''''''''''''''''''''''''##
+
+def add_scfdisp(x):
+    global scfvalue
+    scfvalue = scfvalue + x
+    scfdispval.set(scfvalue)
+
+
+## General elements for all mode ##
+##===============================##
 
 basefrm = Frame(window, height = 40, width = 348, bg = "#383838").pack()
 var = StringVar(basefrm)
@@ -191,29 +235,43 @@ stdfrm = Frame(window, height = 510, width = 350, bg = "#383838")
 scffrm = Frame(window, height = 510, width = 350, bg = "#383838")
 covfrm = Frame(window, height = 510, width = 350, bg = "#383838")
 
+## Elements for standard calculator ##
+##==================================##
 
-oper = Label(stdfrm, textvariable = dispoper, bg = 'green', fg = '#C8C8C8', font = f2)
+oper = Label(stdfrm, textvariable = dispoper, bg = "#383838", fg = '#C8C8C8', font = f2)
 oper.place(x = 320, y = 10)
-display = Label(stdfrm, textvariable = dispval, bd = 0, bg = "blue", anchor = 'e', fg = "white", justify = RIGHT, font = f1)
-display.place(x = 3, y = 35, width =340, height = 50)
+display = Label(stdfrm, textvariable = dispval, bd = 0, bg = "#383838", anchor = 'e', fg = "white", justify = RIGHT, font = f1)
+display.place(x = 3, y = 75, width =340, height = 50)
+stdmemdisp = Label(stdfrm, textvariable = stdmemvar, bg = "#383838", fg = '#C8C8C8', font = f2 )
+stdmemdisp.place(x = 10, y = 10, height = 30, width = 30)
 
-bt7 = Button(stdfrm, text = "7", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda :add_disp("7"))
+stdbtMC = Button(stdfrm, text = "MC", bg = '#383838', fg = 'white', bd = 0, highlightbackground = "#383838", activebackground = '#383838', font = f3, command = lambda : stdcheckmem(1))
+stdbtMC.place(x = 39, y = 165, height = 63, width = 63)
+stdbtMRC = Button(stdfrm, text = "MRC", bg = '#383838', fg = 'white', bd = 0, highlightbackground = "#383838", activebackground = '#383838', font = f3, command = lambda : stdcheckmem(2))
+stdbtMRC.place(x = 108, y = 165, height = 63, width = 63)
+stdbtMadd = Button(stdfrm, text = "M+", bg = '#383838', fg = 'white',bd = 0, highlightbackground = "#383838", activebackground = '#383838', font = f3, command = lambda : stdcheckmem(3))
+stdbtMadd.place(x = 177, y = 165, height = 63, width = 63)
+stdbtMsub = Button(stdfrm, text = "M-", bg = '#383838', fg = 'white', bd = 0, highlightbackground = "#383838", activebackground = '#383838', font = f3, command = lambda : stdcheckmem(4))
+stdbtMsub.place(x = 246, y = 165, height = 63, width = 63)
+
+
+bt7 = Button(stdfrm, text = "7", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda : add_disp("7"))
 bt7.place(x = 3, y = 231, width = 66, height = 66)
-bt8 = Button(stdfrm, text = "8", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda :add_disp("8"))
+bt8 = Button(stdfrm, text = "8", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda : add_disp("8"))
 bt8.place(x = 72, y = 231, width = 66, height = 66)
-bt9 = Button(stdfrm, text = "9", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda :add_disp("9"))
+bt9 = Button(stdfrm, text = "9", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda : add_disp("9"))
 bt9.place(x = 141, y = 231, width = 66, height = 66)
-bt4 = Button(stdfrm, text = "4", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda :add_disp("4"))
+bt4 = Button(stdfrm, text = "4", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda : add_disp("4"))
 bt4.place(x = 3, y = 300, width = 66, height = 66)
-bt5 = Button(stdfrm, text = "5", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda :add_disp("5"))
+bt5 = Button(stdfrm, text = "5", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda : add_disp("5"))
 bt5.place(x = 72, y = 300, width = 66, height = 66)
-bt6 = Button(stdfrm, text = "6", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda :add_disp("6"))
+bt6 = Button(stdfrm, text = "6", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda : add_disp("6"))
 bt6.place(x = 141, y = 300, width = 66, height = 66)
-bt1 = Button(stdfrm, text = "1", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda :add_disp("1"))
+bt1 = Button(stdfrm, text = "1", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda : add_disp("1"))
 bt1.place(x = 3, y = 369, width = 66, height = 66)
-bt2 = Button(stdfrm, text = "2", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda :add_disp("2"))
+bt2 = Button(stdfrm, text = "2", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda : add_disp("2"))
 bt2.place(x = 72, y = 369, width = 66, height = 66)
-bt3 = Button(stdfrm, text = "3", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda :add_disp("3"))
+bt3 = Button(stdfrm, text = "3", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda : add_disp("3"))
 bt3.place(x = 141, y = 369, width = 66, height = 66)
 btDot = Button(stdfrm, text = ".", font = f2, bg = 'black', fg = 'white', bd = 0, highlightbackground = 'black', activebackground = '#787878', command = lambda :add_disp("."))
 btDot.place(x = 3, y = 438, width = 66, height = 66)
@@ -237,9 +295,10 @@ btSub.place(x = 279, y = 369, width = 66, height = 66)
 btEq = Button(stdfrm, text = "=", font = f2, bg = '#db440d', fg = 'white', bd = 0, highlightbackground = '#db440d', activebackground = '#eb6d3f', command = lambda : result(1))
 btEq.place(x = 210, y = 438, width = 135, height = 66)
 
-## Elements for Scientic calculator
+## Elements for Scientic calculator ##
+##==================================##
 
-scfdisplay = Label(scffrm, bg = "green")
+scfdisplay = Label(scffrm, textvariable = scfdispval, bg = "black", fg = 'white', anchor = 'e', justify = RIGHT, font = f3)
 scfdisplay.place(x = 5, y = 0, height = 100, width = 340)
 
 scfbtsin = Button(scffrm, text = "sin", bd = 0, bg = '#555555', fg = 'white', highlightbackground = '#555555')
@@ -272,29 +331,29 @@ scfbtlb.place(x = 119, y = 105, height = 55, width = 55)
 scfbtrb = Button(scffrm, text = ")", bd = 0, bg = '#777777', fg = 'white', highlightbackground = '#777777')
 scfbtrb.place(x = 176, y = 105, height = 55, width = 55)
 
-scfbt7 = Button(scffrm, text = "7", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt7 = Button(scffrm, text = "7", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("7"))
 scfbt7.place(x = 5, y = 276, height = 55, width = 55)
-scfbt8 = Button(scffrm, text = "8", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt8 = Button(scffrm, text = "8", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("8"))
 scfbt8.place(x = 62, y = 276, height = 55, width = 55)
-scfbt9 = Button(scffrm, text = "9", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt9 = Button(scffrm, text = "9", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("9"))
 scfbt9.place(x = 119, y = 276, height = 55, width = 55)
-scfbt4 = Button(scffrm, text = "4", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt4 = Button(scffrm, text = "4", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("4"))
 scfbt4.place(x = 5, y = 333, height = 55, width = 55)
-scfbt5 = Button(scffrm, text = "5", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt5 = Button(scffrm, text = "5", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("5"))
 scfbt5.place(x = 62, y = 333, height = 55, width = 55)
-scfbt6 = Button(scffrm, text = "6", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt6 = Button(scffrm, text = "6", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("6"))
 scfbt6.place(x = 119, y = 333, height = 55, width = 55)
-scfbt1 = Button(scffrm, text = "1", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt1 = Button(scffrm, text = "1", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("1"))
 scfbt1.place(x = 5, y = 390, height = 55, width = 55)
-scfbt2 = Button(scffrm, text = "2", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt2 = Button(scffrm, text = "2", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("2"))
 scfbt2.place(x = 62, y = 390, height = 55, width = 55)
-scfbt3 = Button(scffrm, text = "3", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt3 = Button(scffrm, text = "3", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("3"))
 scfbt3.place(x = 119, y = 390, height = 55, width = 55)
-scfbt0 = Button(scffrm, text = "0", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt0 = Button(scffrm, text = "0", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("0"))
 scfbt0.place(x = 5, y = 447, height = 55, width = 55)
-scfbt00 = Button(scffrm, text = "00", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbt00 = Button(scffrm, text = "00", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("00"))
 scfbt00.place(x = 62, y = 447, height = 55, width = 55)
-scfbtDot = Button(scffrm, text = ".", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2)
+scfbtDot = Button(scffrm, text = ".", bg = "black", fg = "white", bd = 0, highlightbackground = 'black', font = f2, activebackground = '#787878', command = lambda :add_scfdisp("."))
 scfbtDot.place(x = 119, y = 447, height = 55, width = 55)
 
 
@@ -307,13 +366,13 @@ scfbt10x.place(x = 233, y = 219, height = 55, width = 55)
 scfbtPi = Button(scffrm, text = "Pi", bd = 0, bg = '#555555', fg = 'white', highlightbackground = '#555555')
 scfbtPi.place(x = 290, y = 219, height = 55, width = 55)
 
-scfbtx3 = Button(scffrm, text = "X3", bd = 0, bg = '#202020', fg = 'white', highlightbackground = '#202020')
+scfbtx3 = Button(scffrm, text = "x³", bd = 0, bg = '#202020', fg = 'white', highlightbackground = '#202020')
 scfbtx3.place(x = 176, y = 276, height = 55, width = 55)
-scfbt1x = Button(scffrm, text = "1/X", bd = 0, bg = '#202020', fg = 'white', highlightbackground = '#202020')
+scfbt1x = Button(scffrm, text = "¹⁄x", bd = 0, bg = '#202020', fg = 'white', highlightbackground = '#202020')
 scfbt1x.place(x = 233, y = 276, height = 55, width = 55)
-scfbtNF = Button(scffrm, text = "n!", bd = 0, bg = '#202020', fg = 'white', highlightbackground = '#202020')
+scfbtNF = Button(scffrm, text = "x !", bd = 0, bg = '#202020', fg = 'white', highlightbackground = '#202020')
 scfbtNF.place(x = 290, y = 276, height = 55, width = 55)
-scfbtx2 = Button(scffrm, text = "X2", bd = 0, bg = '#202020', fg = 'white', highlightbackground = '#202020')
+scfbtx2 = Button(scffrm, text = "x²", bd = 0, bg = '#202020', fg = 'white', highlightbackground = '#202020')
 scfbtx2.place(x = 176, y = 333, height = 55, width = 55)
 scfbtPerc = Button(scffrm, text = "%", bd = 0, bg = '#202020', fg = 'white', highlightbackground = '#202020')
 scfbtPerc.place(x = 176, y = 390, height = 55, width = 55)
@@ -331,5 +390,7 @@ scfbtSub.place(x = 290, y = 390, height = 55, width = 55)
 scfbtEq = Button(scffrm, text = "=", bg = '#db440d', fg = 'white', highlightbackground = '#db440d', bd = 0, font = f2)
 scfbtEq.place(x = 233, y = 447, height = 55, width = 112)
 
+
+option_changed()
 window.mainloop()
 
