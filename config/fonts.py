88ac4a9ed81ae0a62774c84ef686d85b7f54578b
Bazil Korath <korathbasil@live.com>
2021-07-29 21:38:10 +0530

    create seperate file for fonts, rename fonts with their sizes

---
+++ config/fonts.py
@@ -0,0 +1,8 @@
+from tkinter import font as tkfont
+
+
+size_12 = tkfont.Font(size = 12, weight = 'bold') # 3
+size_14 = tkfont.Font(size = 14, weight = 'bold') # 5
+size_20 = tkfont.Font(size = 20, weight = 'bold') # 2
+size_24 = tkfont.Font(size = 24, weight = 'bold') # 4
+size_34 = tkfont.Font(size = 34, weight = 'bold') # 1
--- models/StandardCalculatorButtons.py
+++ models/StandardCalculatorButtons.py
@@ -1,8 +1,10 @@
 from tkinter import Button
 
+from config import fonts
 from config import colors
 
 
+
 class TypeOne (Button):
-    def __init__(self, text, placement_x, placement_y, command, color_theme_id):
-        super(frame_standard, text = "7", font = f2, bg = color_btlevel3[color_theme], activeforeground = color_text[color_theme], fg = color_text[color_theme], bd = 0, highlightbackground = color_btlevel3[color_theme], activebackground = color_btlevel3[color_theme], command = lambda : add_disp("7"))
+    def __init__(self, frame,text, placement_x, placement_y, command, color_theme_id):
+        super(frame, text = text, font = fonts.size_20, bg = color_btlevel3[color_theme], activeforeground = color_text[color_theme], fg = color_text[color_theme], bd = 0, highlightbackground = color_btlevel3[color_theme], activebackground = color_btlevel3[color_theme], command = lambda : add_disp("7"))
